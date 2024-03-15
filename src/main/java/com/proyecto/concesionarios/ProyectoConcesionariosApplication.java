07bdd847ba72c3ea74320c88f040e7c79890781b
Ismail448 <itsisma448@gmail.com>
2024-03-15 09:09:24 +0100

    first commit

---
+++ src/main/java/com/proyecto/concesionarios/ProyectoConcesionariosApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.concesionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoConcesionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoConcesionariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/concesionarios/ProyectoConcesionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.concesionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoConcesionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
